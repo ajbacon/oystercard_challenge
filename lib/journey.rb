e498c700912e8f78852f88f7932bcee443f80219
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-11-21 15:09:01 +0000

    Expanded and met tests for penalty fare checks for journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -3,15 +3,15 @@ class Journey
   PENALTY_FARE = 6
   MINIMUM_FARE = 1
 
-  def initialize(entry_station)
+  def initialize(entry_station = nil)
     @entry_station = entry_station
     @exit_station = nil
     @fare = PENALTY_FARE
   end
 
-  def end_journey(exit_station)
+  def end_journey(exit_station = nil)
     @exit_station = exit_station
-    @fare = MINIMUM_FARE
+    @fare = MINIMUM_FARE unless entry_station == nil || exit_station == nil
   end
 
 
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,48 +1,68 @@
 require "journey"
 
 describe Journey do
+  context "For a legitimate journey" do
+    let(:station) {double(:station)}
+    subject {Journey.new(station)}
 
-  let(:station) {double(:station)}
-  subject {Journey.new(station)}
+    context "at start of the journey" do
+      describe "#entry_station" do
+        it "Should have an entry station" do
+          expect(subject).to respond_to(:entry_station)
+          expect(subject.entry_station).to eq(station)
+        end
+      end
 
-  context "at start of the journey" do
-    describe "#entry_station" do   
-      it "Should have an entry station" do
-        expect(subject).to respond_to(:entry_station)
-        expect(subject.entry_station).to eq(station)
-      end  
+      it "exit station should be empty" do
+        expect(subject.exit_station).to eq(nil)
+      end
+
+      it "should default to the penalty fare" do
+        expect(subject.fare).to eq(Journey::PENALTY_FARE)
+      end
+
     end
 
-    it "exit station should be empty" do
-      expect(subject.exit_station).to eq(nil)
-    end
+    context "at the end of the journey" do
+      describe "#exit_station" do
 
-    it "should default to the penalty fare" do
-      expect(subject.fare).to eq(Journey::PENALTY_FARE)
-    end
+        before do
+          subject.end_journey(station)
+        end
 
+        it "Should have an exit station" do
+          expect(subject).to respond_to(:exit_station)
+          expect(subject.exit_station).to eq(station)
+        end
+
+        it "should charge the minimum for a sucessful journey" do
+          expect(subject.fare).to eq(Journey::MINIMUM_FARE)
+        end
+
+        # it "should charge a penalty for an improper journey" do
+        #   expect(subject.fare).to eq(Journey::PENALTY_FARE)
+        # end
+      end
+    end
   end
-
-  context "at the end of the journey" do
-    describe "#exit_station" do
-
+  context "For an illegitimate journey" do
+    let(:station) { double(:station)}
+    context "Failure to touch in at start" do
       before do
         subject.end_journey(station)
       end
-
-      it "Should have an exit station" do
-        expect(subject).to respond_to(:exit_station)
-        expect(subject.exit_station).to eq(station)
-      end 
-
-      it "should charge the minimum for a sucessful journey" do
-        expect(subject.fare).to eq(Journey::MINIMUM_FARE)
+      it "should charge a penalty fare" do
+        expect(subject.fare).to eq(Journey::PENALTY_FARE)
+      end
+    end
+    context "Failure to touch out previously" do
+      subject {Journey.new(station)}
+      before do
+        subject.end_journey
+      end
+      it "should charge a penalty fare" do
+        expect(subject.fare).to eq(Journey::PENALTY_FARE)
       end
     end
   end
-
-
-
-
-  
 end
